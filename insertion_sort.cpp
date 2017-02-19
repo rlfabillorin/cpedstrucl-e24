0a94ca6e8528e73695566672f4612dd6126c862f
Ramona Fabillorin <ramonafabillorin@gmail.com>
2017-02-19 20:35:38 +0800

    Update

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,17 +2,30 @@
 using namespace std;
 int main(){
 	int A[]= {40, 30, 20, 50, 10};
-	int i, n, j, temp;
-	for (i=1; i<5; i++){
-		int temp=A[i];
+	int w=1, x, y, z=5;
+	
+	cout<<"Array: ";
+	for(int k=0; k<5; k++)
+		cout<<A[k]<<" ";
+		cout<<endl<<endl;
+	
+	for (int i=1; i<5; i++){
+		int j, temp=A[i];
 		for(j=i-1; j>=0 && temp<A[j]; j--){
 			A[j+1] = A[j];
+			x=A[j];
+			y=temp;
+			
 		}
 		A[j+1] = temp;
+		cout<<endl;
+		cout<<"Swapped: "<<x<<" and "<<y<<endl;
+			
+		cout<<"Pass "<<w++<<": ";
+		for (int k=0; k<z; k++){
+			cout<<A[k]<<" ";
 		}
-	cout<<"Sorted Array: ";
-	for (int i=0; i<5; i++){
-		cout<<A[i]<<" ";
+		cout<<endl;
 	}
 	return 0;
 }
