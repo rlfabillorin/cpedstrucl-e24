88d4a169fd5d9b80a47ad5c01be3e264e3399b5b
Ramona Fabillorin <ramonafabillorin@gmail.com>
2017-02-19 20:32:23 +0800

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,28 +1,32 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
 	int A[]={30, 40, 20, 50, 10};
-	int i, n, j;
-	for(int i=5; i>3; i--)
-	{
+	int n=5, y=1;
+	
+	cout<<"Array: ";
+	for(int k=0; k<5; k++)
+		cout<<A[k]<<" ";
+		cout<<endl<<endl;
+	
+	for(int i=0; i<n-1; i++){
 		int min=A[i], index=i;
-		for(int j=i+1; j<5; j++)
-		{
-			if (A[j] < min)
-			{
+		for(int j=i+1; j<n; j++)
+			if (A[j] < min){
 				min = A[j];
 				index = j;
 			}
-			A[index]=A[i];
-			A[i]=min;
-		}
-	}
+
+		A[index]=A[i];
+		A[i]=min;
+		cout<<endl;
+		cout<<"Swapped: "<<A[index]<<" and "<<A[i]<<endl;
 		
-	cout<<"1st Pass: ";
-	for (int i=0; i<5; i++)
-	{
-		cout<<A[i]<<" ";
+		cout<<"Pass: "<<y++<<": ";
+		for (int i=0; i<5; i++){
+			cout<<A[i]<<" ";
+		}
+		cout<<endl;
 	}
 	return 0;
 }
